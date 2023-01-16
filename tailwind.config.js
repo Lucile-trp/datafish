703bed237ade86a4ec8feab784da29b6fd737855
Lucile TRIPIER <tripier.lucile@gmail.com>
2023-01-16 16:09:16 +0100

    config(tailwind) : lato font configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    fontFamily: {
+      lato: ['"Lato"', "sans"],
+    },
+
+    extend: {
+      colors: {
+        "black": {
+          DEFAULT: "#242424",
+        },
+        "white": {
+          DEFAULT: "#F8F8F8",
+        }
+      },
+    },
   },
   plugins: [],
 }
