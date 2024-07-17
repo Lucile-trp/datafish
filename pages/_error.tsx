acb8be1a0bec5a856026f1513fb0f76828466628
Lucile TRIPIER <tripier.lucile@gmail.com>
2024-07-17 11:32:56 +0200

    feat(pages) : add error page

---
+++ pages/_error.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Error() {
+  return (
+    <h1 className="text-white text-2xl lg:w-2-4">Ne vous aventurez pas dans les profondeurs inconnues. Des créatures y rôdent...</h1>
+  )
+}
