239a7f89a88abbe102c8a1777571a23162d1a9a0
Lucile TRIPIER <tripier.lucile@gmail.com>
2024-07-17 11:12:01 +0200

    style(card) : add style card

--- components/ui/utilities/Card.tsx
+++ components/ui/utilities/Card.tsx
@@ -1,11 +1,11 @@
 import { Fish } from "@prisma/client";
 
-export const Card = (fish:Fish) => {
-    console.log(fish)
+export const Card = (fish: Fish) => {
+  console.log(fish);
   return (
-    <div className="bottom-0 w-full h-[50px]">
-        <p>{fish.name}</p>
-
+    <div className="rounded border border-white/20 p-4 hover:border-white cursor-pointer">
+      <h3 className="text-white text-2xl">{fish.name}</h3>
+      <p className="text-white italic">{fish.vernacular_name || 'N/A'}</p>
     </div>
   );
 };
