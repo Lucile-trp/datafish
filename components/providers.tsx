8f35384b3db296c18eecbb2cf2c5d6d105a88dcd
Lucile TRIPIER <tripier.lucile@gmail.com>
2024-09-11 16:25:02 +0200

    feat(cookies): add cookies provider

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,13 +1,20 @@
 import { ReactNode, useEffect } from "react";
 import { SessionProvider } from "next-auth/react";
 import { Session } from "next-auth";
+import { CookiesProvider } from "react-cookie";
 
-export const Providers = ({ children, session }: { children: ReactNode, session?: Session | null }) => {
+export const Providers = ({
+  children,
+  session,
+}: {
+  children: ReactNode;
+  session?: Session | null;
+}) => {
   return (
     <>
-      <SessionProvider session={session}>
-        {children}
-      </SessionProvider>
+      <CookiesProvider>
+        <SessionProvider session={session}>{children}</SessionProvider>
+      </CookiesProvider>
     </>
   );
 };
