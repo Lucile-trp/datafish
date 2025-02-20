cb4d525c94844124a62037383db2f28fa0c6dcad
Lucile TRIPIER <tripier.lucile@gmail.com>
2025-02-20 12:47:18 +0100

    feat(type): add first version of Fish type

---
+++ types/Fish.ts
@@ -0,0 +1,56 @@
+export type Fish = {
+  metadata: {
+    id: string;
+    created_at: Date;
+    updated_at?: Date | null;
+  };
+
+  // MAIN INFORMATIONS
+  commun_name: string;
+  scientific_name: string;
+  alternative_names?: string[];
+  created_by: string;
+  updated_by?: string | null;
+  status: string; // Status of the post
+  picture?: string | null;
+  description: string;
+
+  // TAXONOMY
+  classification: {
+    domain: string;
+    kingdom: string;
+    subkingdom?: string | null;
+    infrakingdom?: string | null;
+    phylum: string;
+    subphylum?: string | null;
+    class: string;
+    order: string;
+    family: string;
+    subfamily?: string | null;
+    genus: string;
+    species: string;
+  };
+
+  // STATUS
+  UICN_status: string;
+
+  //LOCALISATION & ENVIRONNEMENT
+  water_type: string;
+  depth_range: { min: number; max: number };
+  temperature_range: { min: number; max: number };
+  pH_range: { min: number; max: number };
+
+  //PHYSICAL
+  length_range: { min: number; max: number };
+  height_range: { min: number; max: number };
+
+  //LIFESTYLE
+  venoumous: boolean;
+  gestation: number;
+  sociability: string;
+  diet: string;
+  lifespan: number;
+
+  // AQUARIOPHILY
+  difficulty: string;
+};
