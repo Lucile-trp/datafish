97f1cad4826342795ab73d5091054c266c984cd7
Lucile TRIPIER <tripier.lucile@gmail.com>
2025-04-01 15:23:55 +0200

    fix(import): delete unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/UI/Header";
 import { Footer } from "@/components/UI/Footer";
