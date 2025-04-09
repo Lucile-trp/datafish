00d7c37c91367fdb858af8a4a9f50ba15e1fdd1a
Lucile TRIPIER <tripier.lucile@gmail.com>
2025-04-09 10:28:02 +0200

    feat(auth): bubble party

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,8 +1,10 @@
+import BubblesBackground from "@/components/UI/Background_bubbles";
 import { AuthForm } from "@/components/Users/AuthForm";
 
 export default function Auth() {
   return (
     <div className="grid min-h-screen">
+      <BubblesBackground></BubblesBackground>
       <main className="justify-center items-center flex">
         <AuthForm></AuthForm>
       </main>
