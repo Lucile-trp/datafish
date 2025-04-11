0a1386f1f782f65a910aa9ac7d72ae6f7e51daad
Lucile TRIPIER <tripier.lucile@gmail.com>
2025-04-11 10:20:59 +0200

    feat(auth): register api

---
+++ app/api/v1/auth/register/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+import { hash } from "bcrypt";
+
+export async function POST(req: Request) {
+  const { pseudo, email, password } = await req.json();
+
+  if (!email || !password || !pseudo) {
+    return NextResponse.json(
+      { success: false, message: "Champs manquants." },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const hashedPassword = await hash(password, 10);
+    const userPayload = {
+      id: crypto.randomUUID(),
+      name: pseudo,
+      email,
+      password: hashedPassword,
+      roles: ["USER"],
+    };
+
+    const response = await fetch(`${process.env.URL_LOCAL}/api/v1/users`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(userPayload),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      return NextResponse.json(data, { status: response.status });
+    }
+
+    return NextResponse.json({ success: true, user: data.user });
+  } catch (err) {
+    console.error("Erreur d'inscription :", err);
+    return NextResponse.json(
+      { success: false, message: "Erreur serveur pendant l'inscription." },
+      { status: 500 }
+    );
+  }
+}
