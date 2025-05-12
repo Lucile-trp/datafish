3582e99171966b5dbd4a72e42eb8e1777b93d65c
Gregory <gregory.leblond.dev@gmail.com>
2025-05-12 14:00:05 +0200

    fix(middleware) allow request from vercel dev url

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,14 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
     const origin = request.nextUrl.origin
     const userAgent = request.headers.get('user-agent')
+    const vercelUrlPattern = new RegExp("^https://datafish-[\\w-]+-luciletrps-projects\\.vercel\\.app/?$");
+
 
     if(origin === "http://localhost:3000" && userAgent && userAgent.startsWith("PostmanRuntime")){
         return NextResponse.next()
     }
 
-    if(origin === "https://datafish.vercel.app/") {
+    if(origin === "https://datafish.vercel.app/" || vercelUrlPattern.test(origin)) {
         return NextResponse.next()
     }
 
